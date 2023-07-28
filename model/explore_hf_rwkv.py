8bc335e0d221abfb91339b9864121cc741832814
felixdae <felixdae@gmail.com>
2023-07-28 13:22:58 +0800

    add rwkv

--- model/explore_hf_rwkv.py
+++ model/explore_hf_rwkv.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 def main():
-    model = AutoModelForCausalLM.from_pretrained("RWKV/rwkv-4-430m-pile").to(0)
+    model = AutoModelForCausalLM.from_pretrained("RWKV/rwkv-4-430m-pile")
     tokenizer = AutoTokenizer.from_pretrained("RWKV/rwkv-4-430m-pile")
 
     prompt = "\nhello"
@@ -11,8 +11,6 @@ def main():
     inputs = tokenizer(prompt, return_tensors="pt")
     output = model.generate(inputs["input_ids"], max_new_tokens=40)
     print(tokenizer.decode(output[0].tolist(), skip_special_tokens=True))
-
-
 
 
 if __name__ == '__main__':
