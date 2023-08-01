6847d00110c11a2013d1d7f20f5573020b603b23
felixdae <felixdae@gmail.com>
2023-08-01 19:23:27 +0800

    remove test

--- tests/chatglm_test.py
+++ tests/chatglm_test.py
@@ -31,25 +31,3 @@ class ModelTest(unittest.TestCase):
             t2 = layer_output[i]
             delta = torch.abs(torch.max(t2 - t1))
             self.assertTrue(delta < 1e-3, f"fail at layer {i}, delta {delta}")
-
-    def test_modeling_gpu(self):
-        ref_model_id = "felixdae/chatglm-6b"
-        ref_model = AutoModel.from_pretrained(ref_model_id, trust_remote_code=True)
-        ref_model = ref_model.half().cuda()
-
-        config = ChatGLMConfig(num_layers=2)
-        model = Model(config)
-        model.load_weights_from_hf(ref_model_id)
-
-        input_ids, position_ids = torch.LongTensor([42, 130001, 130004]), torch.LongTensor([[0, 1, 1], [0, 0, 1]])
-        out1 = ref_model(input_ids.unsqueeze(0).cuda(), position_ids.unsqueeze(0).cuda(), output_hidden_states=True)
-        out2, layer_output = model(input_ids, position_ids)
-
-        delta = torch.abs(torch.max(out1.hidden_states[-1].cpu().transpose(0, 1)[0] - out2))
-        self.assertTrue(delta < 1e-3, f"fail at final output, delta {delta}")
-
-        for i in range(config.num_layers):
-            t1 = out1.hidden_states[i].transpose(0, 1)[0]
-            t2 = layer_output[i]
-            delta = torch.abs(torch.max(t2 - t1))
-            self.assertTrue(delta < 1e-3, f"fail at layer {i}, delta {delta}")
