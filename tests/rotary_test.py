e93fca9b2a3984ff509f8601e30a30ef94677e7b
felixdae <felixdae@gmail.com>
2023-08-01 18:40:28 +0800

    test chatglm2 rotary

--- tests/rotary_test.py
+++ tests/rotary_test.py
@@ -1,8 +1,8 @@
 import unittest
 
 import torch
-
 import torch.nn.functional as F
+from torch import nn
 
 from model.common import Rotary
 
@@ -121,6 +121,66 @@ class LlamaRotaryEmbedding(torch.nn.Module):
         return q_embed, k_embed
 
 
+class ChatGLM2RotaryEmbedding(nn.Module):
+    def __init__(self, dim, original_impl=False, device=None, dtype=None):
+        super().__init__()
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2, device=device).to(dtype=dtype) / dim))
+        self.register_buffer("inv_freq", inv_freq)
+        self.dim = dim
+        self.original_impl = original_impl
+
+    def forward_impl(
+            self, seq_len: int, n_elem: int, dtype: torch.dtype, device: torch.device, base: int = 10000
+    ):
+        """Enhanced Transformer with Rotary Position Embedding.
+
+        Derived from: https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/
+        transformers/rope/__init__.py. MIT License:
+        https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/license.
+        """
+        # $\Theta = {\theta_i = 10000^{\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
+        theta = 1.0 / (base ** (torch.arange(0, n_elem, 2, dtype=dtype, device=device) / n_elem))
+
+        # Create position indexes `[0, 1, ..., seq_len - 1]`
+        seq_idx = torch.arange(seq_len, dtype=dtype, device=device)
+
+        # Calculate the product of position index and $\theta_i$
+        idx_theta = torch.outer(seq_idx, theta).float()
+
+        cache = torch.stack([torch.cos(idx_theta), torch.sin(idx_theta)], dim=-1)
+
+        # this is to mimic the behaviour of complex32, else we will get different results
+        if dtype in (torch.float16, torch.bfloat16, torch.int8):
+            cache = cache.bfloat16() if dtype == torch.bfloat16 else cache.half()
+        return cache
+
+    def forward(self, max_seq_len, offset=0):
+        return self.forward_impl(
+            max_seq_len, self.dim, dtype=self.inv_freq.dtype, device=self.inv_freq.device
+        )
+
+    @staticmethod
+    def apply_rotary_pos_emb(x: torch.Tensor, rope_cache: torch.Tensor) -> torch.Tensor:
+        # x: [sq, b, np, hn]
+        sq, b, np, hn = x.size(0), x.size(1), x.size(2), x.size(3)
+        rot_dim = rope_cache.shape[-2] * 2
+        x, x_pass = x[..., :rot_dim], x[..., rot_dim:]
+        # truncate to support variable sizes
+        rope_cache = rope_cache[:sq]
+        xshaped = x.reshape(sq, -1, np, rot_dim // 2, 2)
+        rope_cache = rope_cache.view(sq, -1, 1, xshaped.size(3), 2)
+
+        x_out2 = torch.stack(
+            [
+                xshaped[..., 0] * rope_cache[..., 0] - xshaped[..., 1] * rope_cache[..., 1],
+                xshaped[..., 1] * rope_cache[..., 0] + xshaped[..., 0] * rope_cache[..., 1],
+            ],
+            -1,
+        )
+        x_out2 = x_out2.flatten(3)
+        return torch.cat((x_out2, x_pass), dim=-1)
+
+
 class RotaryTest(unittest.TestCase):
 
     def test_gold(self):
@@ -155,3 +215,18 @@ class RotaryTest(unittest.TestCase):
             tou = my.apply(i, y3[i])
             self.assertTrue(torch.max(torch.abs(q1.squeeze()[i] - out.squeeze())) < 1e-3)
             self.assertTrue(torch.max(torch.abs(k1.squeeze()[i] - tou.squeeze())) < 1e-3)
+
+    def test_chatglm2(self):
+        torch.manual_seed(0)
+        dim = 128
+        rot = ChatGLM2RotaryEmbedding(dim // 2)
+        seq = 5
+        x = torch.normal(0, 1, (seq, 1, 1, dim))
+        cache = rot(max_seq_len=10)
+        y = ChatGLM2RotaryEmbedding.apply_rotary_pos_emb(x, cache)
+
+        for i in range(seq):
+            mr = Rotary(d=dim // 2)
+            z = mr.apply(i, x[i, 0, 0, :dim // 2])
+            self.assertTrue(torch.max(torch.abs(z - y[i, 0, 0, :dim // 2])) < 1e-3)
+            self.assertTrue(torch.max(torch.abs(x[i, 0, 0, dim // 2:] - y[i, 0, 0, dim // 2:])) < 1e-6)
