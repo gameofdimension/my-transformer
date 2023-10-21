a82eadece5e09f9b583915e36be781f6c2d9ce88
felixdae <felixdae@gmail.com>
2023-10-21 18:14:20 +0800

    fix cases

--- flash/incremental.py
+++ flash/incremental.py
@@ -50,56 +50,56 @@ def incremental_softmax_weighted_sum_2d(score: np.ndarray, value: np.ndarray, bl
     assert score.ndim == value.ndim == 2
     assert score.shape[1] == value.shape[0]
 
-    def compute_one_block(vec: np.ndarray, val: np.ndarray):
-        assert 0 < vec.shape[1] <= block_size
-        assert vec.shape[1] == val.shape[0]
-        maxv = vec.max(axis=1, keepdims=True)
+    def compute_one_block(s: np.ndarray, val: np.ndarray):
+        assert 0 < s.shape[1] <= block_size
+        assert s.shape[1] == val.shape[0]
+        max_expo = s.max(axis=1, keepdims=True)
 
-        vec = vec - maxv
-        exp_val = np.exp(vec)
-        total = np.sum(exp_val, axis=1, keepdims=True)
-        return maxv, exp_val @ val, total
+        s = s - max_expo
+        exp_val = np.exp(s)
+        denominator = np.sum(exp_val, axis=1, keepdims=True)
+        return max_expo, exp_val @ val, denominator
 
     def merge_two_block(
-            v1: np.ndarray, l1: np.ndarray, m1: np.ndarray,
-            v2: np.ndarray, l2: np.ndarray, m2: np.ndarray):
-        maxv = np.maximum(m1, m2)
-        total = l1 * np.exp(m1 - maxv) + l2 * np.exp(m2 - maxv)
+            numerator1: np.ndarray, denominator1: np.ndarray, expo1: np.ndarray,
+            numerator2: np.ndarray, denominator2: np.ndarray, expo2: np.ndarray):
+        max_expo = np.maximum(expo1, expo2)
+        total = denominator1 * np.exp(expo1 - max_expo) + denominator2 * np.exp(expo2 - max_expo)
 
-        v1 = np.exp(m1 - maxv) * v1
-        v2 = np.exp(m2 - maxv) * v2
-        return maxv, v1 + v2, total
+        numerator1 = np.exp(expo1 - max_expo) * numerator1
+        numerator2 = np.exp(expo2 - max_expo) * numerator2
+        return max_expo, numerator1 + numerator2, total
 
     def iterative_merge():
-        m = np.full((score.shape[0], 1), -float('inf'))
-        e = np.zeros((score.shape[0], value.shape[1]))
-        t = np.zeros((score.shape[0], 1))
+        max_e = np.full((score.shape[0], 1), -float('inf'))
+        num = np.zeros((score.shape[0], value.shape[1]))
+        denom = np.zeros((score.shape[0], 1))
 
         i = 0
         while True:
             if i + block_size >= score.shape[1]:
-                nm, ne, nt = compute_one_block(score[:, i:], value[i:, :])
-                _, e, t = merge_two_block(e, t, m, ne, nt, nm)
-                return e / t
+                new_max_e, new_num, new_denom = compute_one_block(score[:, i:], value[i:, :])
+                _, num, denom = merge_two_block(num, denom, max_e, new_num, new_denom, new_max_e)
+                return num / denom
 
-            nm, ne, nt = compute_one_block(score[:, i:i + block_size], value[i:i + block_size, :])
-            m, e, t = merge_two_block(e, t, m, ne, nt, nm)
+            new_max_e, new_num, new_denom = compute_one_block(score[:, i:i + block_size], value[i:i + block_size, :])
+            max_e, num, denom = merge_two_block(num, denom, max_e, new_num, new_denom, new_max_e)
             i += block_size
 
     def merge_all_at_once():
         lst = []
-        maxm = np.full((score.shape[0], 1), -float('inf'))
+        max_expo = np.full((score.shape[0], 1), -float('inf'))
         for i in range(0, score.shape[1], block_size):
             m, n, d = compute_one_block(score[:, i:i + block_size], value[i:i + block_size, :])
             lst.append((m, n, d))
-            assert maxm.shape == m.shape
-            maxm = np.maximum(maxm, m)
+            assert max_expo.shape == m.shape
+            max_expo = np.maximum(max_expo, m)
 
         ns = np.zeros((score.shape[0], value.shape[1]))
         ds = np.zeros((score.shape[0], 1))
         for m, n, d in lst:
-            ns += np.exp(m - maxm) * n
-            ds += np.exp(m - maxm) * d
+            ns += np.exp(m - max_expo) * n
+            ds += np.exp(m - max_expo) * d
 
         return ns / ds
 
