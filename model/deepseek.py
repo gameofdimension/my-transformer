53565f88be75dcb31a29f00fa0e6525d32d1ba66
felixdae <felixdae@gmail.com>
2024-01-26 08:06:21 +0000

    add deepseek moe

---
+++ model/deepseek.py
@@ -0,0 +1,211 @@
+import torch
+from torch import nn
+from transformers.activations import ACT2FN
+
+from model.common import RMSNorm, apply_rotary, precompute_cos_sin
+from model.deepseek_config import DeepseekConfig
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, config: DeepseekConfig, layer_idx: int,
+                 get_cos_sin):
+        super().__init__()
+        self.layer_idx = layer_idx
+        self.hidden_size = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.head_dim = self.hidden_size // self.num_heads
+        self.num_key_value_heads = config.num_key_value_heads
+        assert self.num_key_value_heads == self.num_heads
+        self.num_key_value_groups = self.num_heads // self.num_key_value_heads
+        self.max_position_embeddings = config.max_position_embeddings
+        self.rope_theta = config.rope_theta
+
+        self.q_proj = nn.Linear(
+            self.hidden_size, self.num_heads * self.head_dim,
+            bias=config.attention_bias)
+        self.k_proj = nn.Linear(
+            self.hidden_size, self.num_key_value_heads * self.head_dim,
+            bias=config.attention_bias)
+        self.v_proj = nn.Linear(
+            self.hidden_size, self.num_key_value_heads * self.head_dim,
+            bias=config.attention_bias)
+        self.o_proj = nn.Linear(
+            self.num_heads * self.head_dim, self.hidden_size,
+            bias=config.attention_bias)
+        cos_sin = get_cos_sin()
+        self.cos = cos_sin[0]
+        self.sin = cos_sin[1]
+
+    def forward(self, hidden_states: torch.Tensor):
+        assert len(hidden_states.size()) == 3
+        bsz, q_len, hsz = hidden_states.size()
+        assert hsz == self.hidden_size
+        all_q = self.q_proj(hidden_states).view(
+            bsz, q_len, self.num_heads, self.head_dim).permute(1, 0, 2, 3)
+        all_k = self.k_proj(hidden_states).view(
+            bsz, q_len, self.num_key_value_heads, self.head_dim)\
+            .repeat_interleave(self.num_key_value_groups, dim=2)\
+            .permute(1, 0, 2, 3)
+        all_v = self.v_proj(hidden_states).view(
+            bsz, q_len, self.num_key_value_heads, self.head_dim)\
+            .repeat_interleave(self.num_key_value_groups, dim=2)\
+            .permute(0, 2, 1, 3)
+
+        cos, sin = self.cos[:q_len], self.sin[:q_len]
+        all_q = apply_rotary(all_q, cos, sin).permute(1, 2, 0, 3)
+        all_k = apply_rotary(all_k, cos, sin).permute(1, 2, 0, 3)
+
+        out = nn.functional.scaled_dot_product_attention(
+            query=all_q, key=all_k, value=all_v, is_causal=True)
+
+        out = out.permute(0, 2, 1, 3).reshape(bsz, q_len, -1)
+        out = self.o_proj(out)
+        return out
+
+
+class MLP(nn.Module):
+    def __init__(
+            self, hidden_act: str, hidden_size: int, intermediate_size: int):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+
+        self.gate_proj = nn.Linear(
+            self.hidden_size, self.intermediate_size, bias=False)
+        self.up_proj = nn.Linear(
+            self.hidden_size, self.intermediate_size, bias=False)
+        self.down_proj = nn.Linear(
+            self.intermediate_size, self.hidden_size, bias=False)
+        self.act_fn = ACT2FN[hidden_act]
+
+    def forward(self, hidden_states: torch.Tensor):
+        out = self.down_proj(self.act_fn(
+            self.gate_proj(hidden_states)) * self.up_proj(hidden_states))
+        return out
+
+
+class Gate(nn.Module):
+    def __init__(self, config: DeepseekConfig):
+        super().__init__()
+        self.linear = nn.Linear(
+            config.hidden_size, config.n_routed_experts, bias=False)
+        assert config.scoring_func == 'softmax'
+        self.top_k = config.num_experts_per_tok
+
+    def forward(self, hidden_states: torch.Tensor):
+        logits = self.linear(hidden_states)
+        probs = nn.functional.softmax(logits, dim=-1)
+
+        routing_weights, selected_experts = torch.topk(
+            probs, self.top_k, dim=-1)
+        routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+
+        return routing_weights, selected_experts
+
+
+class Moe(nn.Module):
+    def __init__(self, config: DeepseekConfig):
+        super().__init__()
+        self.config = config
+        self.num_experts_per_tok = config.num_experts_per_tok
+        self.experts = nn.ModuleList([MLP(
+            hidden_act=config.hidden_act,
+            hidden_size=config.hidden_size,
+            intermediate_size=config.moe_intermediate_size)
+            for i in range(config.n_routed_experts)])
+        self.gate = Gate(config)
+        assert config.n_shared_experts is not None
+        intermediate_size = config.moe_intermediate_size * \
+            config.n_shared_experts
+        self.shared_experts = MLP(
+            hidden_act=config.hidden_act,
+            hidden_size=config.hidden_size,
+            intermediate_size=intermediate_size,
+        )
+
+    def forward(self, hidden_states: torch.Tensor):
+        bsz, seq_len, hidden_dim = hidden_states.size()
+        hidden_states = hidden_states.view(-1, hidden_dim)
+        routing_weights, selected_experts = self.gate(hidden_states)
+
+        assert hidden_states.size(0) == routing_weights.size(
+            0) == selected_experts.size(0)
+        out = torch.zeros_like(hidden_states)
+        for i in range(hidden_states.size(0)):
+            out[i] += self.shared_experts(hidden_states[i])
+            for weight, idx in zip(routing_weights[i], selected_experts[i]):
+                expert = self.experts[idx]
+                out[i] += weight*expert(hidden_states[i])
+        return out.reshape(bsz, seq_len, hidden_dim)
+
+
+class Block(nn.Module):
+    def __init__(
+            self, config: DeepseekConfig, layer_idx: int,
+            get_cos_sin):
+        super().__init__()
+        self.input_layernorm = RMSNorm(
+            config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(
+            config.hidden_size, eps=config.rms_norm_eps)
+        self.self_attn = SelfAttention(
+            config=config, layer_idx=layer_idx,
+            get_cos_sin=get_cos_sin)
+        assert config.n_routed_experts is not None
+        if layer_idx >= config.first_k_dense_replace and \
+                layer_idx % config.moe_layer_freq == 0:
+            self.mlp = Moe(config)
+        else:
+            self.mlp = MLP(
+                hidden_act=config.hidden_act, hidden_size=config.hidden_size,
+                intermediate_size=config.intermediate_size)
+
+    def forward(self, hidden_states: torch.Tensor):
+        residual = hidden_states
+        hidden_states = self.input_layernorm(hidden_states)
+        hidden_states = self.self_attn(hidden_states)
+        hidden_states = residual + hidden_states
+
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states = self.mlp(hidden_states)
+        hidden_states = residual + hidden_states
+        return hidden_states
+
+
+class Model(nn.Module):
+    def __init__(self, config: DeepseekConfig):
+        super().__init__()
+        self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        padding_idx = getattr(config, "pad_token_id", None)
+        self.embed_tokens = nn.Embedding(
+            config.vocab_size, config.hidden_size, padding_idx)
+        get_cos_sin = precompute_cos_sin(
+            config.rope_theta,
+            config.max_position_embeddings,
+            config.hidden_size//config.num_attention_heads,
+            config.device)
+        self.layers = nn.ModuleList(
+            [Block(config, layer_idx, get_cos_sin)
+             for layer_idx in range(config.num_hidden_layers)]
+        )
+
+    def forward(self, input_ids: torch.LongTensor):
+        hidden_states = self.embed_tokens(input_ids)
+        layers_output = [hidden_states.detach()]
+        for layer in self.layers:
+            hidden_states = layer(hidden_states)
+            layers_output.append(hidden_states.detach())
+        return self.norm(hidden_states), layers_output
+
+
+if __name__ == "__main__":
+    # from transformers import AutoModelForCausalLM
+    # model = AutoModelForCausalLM.from_pretrained(
+    #     "deepseek-ai/deepseek-moe-16b-chat", trust_remote_code=True)
+    # print(model)
+    config = DeepseekConfig(device='cpu')
+    model = Model(config)
+    data = torch.randint(0, config.vocab_size, (3, 4))
+
+    model(data)
---
+++ model/deepseek_config.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DeepseekConfig:
+    vocab_size: int = 102400
+    hidden_act: str = "silu"
+    hidden_size: int = 2048
+    intermediate_size: int = 10944
+    max_position_embeddings: int = 4096
+    moe_intermediate_size: int = 1408
+    n_routed_experts: int = 64
+    n_shared_experts: int = 2
+    num_attention_heads: int = 16
+    num_experts_per_tok: int = 6
+    num_hidden_layers: int = 28
+    num_key_value_heads: int = 16
+    rms_norm_eps: float = 1e-06
+    device: str = 'cuda'
+    attention_bias: bool = False
+    scoring_func: str = 'softmax'
+    rope_theta: int = 10000
+    moe_layer_freq: int = 1
+    first_k_dense_replace: int = 1
